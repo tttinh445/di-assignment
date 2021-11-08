bc326b863f4019f238fc39af4aa73b1eb2d14957
tan nguyen tan <tanbinhnguyen1122@gmail.com>
2021-11-08 16:48:50 +0700

    feat: home

--- routes/web/index.js
+++ routes/web/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 router.use('/users', require('./users'));
 router.use('/auth', require('./auth'));
 
+router.get("/", (req, res) => {
+  return res.redirect('/users');
+});
+
 router.use(function (err, req, res, next) {
   return next(err)
 });
